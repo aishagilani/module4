ee5cfe1089c79d66b981869bce43278064db92b9
aisha gilani <aishaagilani@gmail.com>
2025-11-14 16:59:07 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,30 @@
+class Employee {
+    String name;
+    double salary;
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+}
+class Manager extends Employee {
+    String department;
+    Manager(String name, double salary, String department) {
+        super(name, salary);
+        this.department = department;
+    }
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Manager m = new Manager("Sam", 80000, "HR");
+        m.displayInfo();
+    }
+}
+
