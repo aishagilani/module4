2a971538b18eb114292f8d2338bb502d5e7c31a6
aisha gilani <aishaagilani@gmail.com>
2025-12-02 16:05:07 +0530

    Create bank.java

---
+++ bank.java
@@ -0,0 +1,20 @@
+public class bank {
+    public static void main(String[] args) {
+        Bank b1 = new SBI();
+        Bank b2 = new HDFC();
+        System.out.println(b1.rateOfInterest());
+        System.out.println(b2.rateOfInterest());
+    }
+}
+
+interface Bank {
+    int rateOfInterest();
+}
+
+class SBI implements Bank {
+    public int rateOfInterest() { return 6; }
+}
+
+class HDFC implements Bank {
+    public int rateOfInterest() { return 8; }
+}
